6459a08ff6b55823e0d5c2a4dea5586b854bb88f
bhgs89 <han.baek.program@gmail.com>
2019-08-10 00:11:56 -0700

    Adjust mainpage for pixel xl - v5

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -28,13 +28,13 @@ const MenuBar = styled.div`
     z-index: 50;
     width: 80%;
 
-    @media only screen and (min-device-width: 411px) and (orientation: portrait) {
+    @media only screen and (min-device-width: 411px) and (max-device-width: 768px) and (orientation: portrait) {
       margin: 0;
       top: 10px;
       left: 30px;
       width: 350px;
     }
-    @media only screen and (min-device-width: 731px) and (orientation: landscape) {
+    @media only screen and (min-device-width: 731px) and (max-device-width: 768px) and (orientation: landscape) {
       margin: 0;
       top: 10px;
       left: 40px;
--- src/components/mainpage/ContentA.js
+++ src/components/mainpage/ContentA.js
@@ -9,20 +9,20 @@ const ContentContainer = styled.div`
   padding-bottom: 70px;
   text-align: center;
   height: 470px;
-  @media only screen and (min-device-width: 411px) and (orientation: portrait) {
+  @media only screen and (min-device-width: 411px) and (max-device-width: 768px) and (orientation: portrait) {
     padding-top: 30px;
   }
-  @media only screen and (min-device-width: 731px) and (orientation: landscape) {
+  @media only screen and (min-device-width: 731px) and (max-device-width: 768px) and (orientation: landscape) {
     padding-top: 30px;
   }
 `;
 const Header = styled.h1`
   text-transform: uppercase;
   font-size: 250%;
-  @media only screen and (min-device-width: 411px) and (orientation: portrait) {
+  @media only screen and (min-device-width: 411px) and (max-device-width: 768px) and (orientation: portrait) {
     font-size: 15px;
   }
-  @media only screen and (min-device-width: 731px) and (orientation: landscape) {
+  @media only screen and (min-device-width: 731px) and (max-device-width: 768px) and (orientation: landscape) {
     font-size: 25px;
   }
 `;
@@ -36,13 +36,13 @@ const PhylosophyContent = styled.p`
   font-size: 19px;
   letter-spacing: 2px;
 
-  @media only screen and (min-device-width: 411px) and (orientation: portrait) {
+  @media only screen and (min-device-width: 411px) and (max-device-width: 768px) and (orientation: portrait) {
     text-align: center;
     text-indent: 0;
     font-size: 12px;
     letter-spacing: 1px;
   }
-  @media only screen and (min-device-width: 731px) and (orientation: landscape) {
+  @media only screen and (min-device-width: 731px) and (max-device-width: 768px) and (orientation: landscape) {
     text-align: center;
     text-indent: 0;
     font-size: 15px;
