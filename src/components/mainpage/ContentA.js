6d8607fb491e2a7caa8acbb4a599d47ec6387358
bhgs89 <han.baek.program@gmail.com>
2019-08-09 23:51:20 -0700

    Adjust mainpage for pixel xl - v3

--- src/components/mainpage/ContentA.js
+++ src/components/mainpage/ContentA.js
@@ -9,10 +9,22 @@ const ContentContainer = styled.div`
   padding-bottom: 70px;
   text-align: center;
   height: 470px;
+  @media only screen and (min-device-width: 411px) and (orientation: portrait) {
+    padding-top: 30px;
+  }
+  @media only screen and (min-device-width: 731px) and (orientation: landscape) {
+    padding-top: 30px;
+  }
 `;
-const Heder = styled.h1`
+const Header = styled.h1`
   text-transform: uppercase;
   font-size: 250%;
+  @media only screen and (min-device-width: 411px) and (orientation: portrait) {
+    font-size: 15px;
+  }
+  @media only screen and (min-device-width: 731px) and (orientation: landscape) {
+    font-size: 25px;
+  }
 `;
 const Phylosophy = styled.div`
   width: 70%;
@@ -23,21 +35,33 @@ const PhylosophyContent = styled.p`
   text-indent: 25px;
   font-size: 19px;
   letter-spacing: 2px;
+
+  @media only screen and (min-device-width: 411px) and (orientation: portrait) {
+    text-indent: 0;
+    font-size: 12px;
+    letter-spacing: 1px;
+  }
+  @media only screen and (min-device-width: 731px) and (orientation: landscape) {
+    text-indent: 0;
+    font-size: 15px;
+    letter-spacing: 1px;
+  }
 `;
 // Style End
 
 const ContentA = () => {
   return (
     <ContentContainer>
-      <Heder>Computer philosophie</Heder>
+      <Header>Computer philosophie</Header>
       <Phylosophy>
         <PhylosophyContent>
-          My computer philosophie is that computer science is necessary to improve our lives. It
-          will be more important in the future, because we cannot live without its system. The most
-          people only know how to use their software. They don't know how it works, because they
-          don't need to know it. Knowing how it works is our (Programmers) job! Therefore, we need
-          to keep study our field. To make better world! To make better life! My study is going to
-          be endless.
+          My computer philosophie is that computer science is necessary to
+          improve our lives. It will be more important in the future, because we
+          cannot live without its system. The most people only know how to use
+          their software. They don't know how it works, because they don't need
+          to know it. Knowing how it works is our (Programmers) job! Therefore,
+          we need to keep study our field. To make better world! To make better
+          life! My study is going to be endless.
         </PhylosophyContent>
       </Phylosophy>
     </ContentContainer>
